a8bfb383816021eba68c56d1c85b7fd9bd76cc36
LuiD2 <47342745+LuiD2@users.noreply.github.com>
2021-08-23 19:14:04 -0600

    Menu stuff

--- menus/views.py
+++ menus/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from menus.models import Menu
 from django.http import Http404
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+
 
 # Create your views here.
 class IndexView(generic.ListView):
@@ -9,10 +11,18 @@ class IndexView(generic.ListView):
     template_name = 'menus/index.html'
     context_object_name = 'menus'
 
+
 class DetailView(generic.DetailView):
     model = Menu
     template_name = 'menus/detail.html'
 
+
 class MenuRecipeDetailView(generic.DetailView):
     model = Menu
     template_name = 'menus/MenuRecipeDetail.html'
+
+
+class MenuCreateView(CreateView):
+    model = Menu
+    template_name = 'menus/add_menu.html'
+    fields = ['name', 'recipes', 'servings']
