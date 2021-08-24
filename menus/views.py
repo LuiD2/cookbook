e1b9c10eafb65eb162cd8f5a637d7058862ef7e7
LuiD2 <47342745+LuiD2@users.noreply.github.com>
2021-08-23 19:14:04 -0600

    Menu stuff

--- menus/views.py
+++ menus/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from menus.models import Menu
 from django.http import Http404
-from menus.forms import ContactForm
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 
 # Create your views here.
@@ -11,23 +11,18 @@ class IndexView(generic.ListView):
     template_name = 'menus/index.html'
     context_object_name = 'menus'
 
+
 class DetailView(generic.DetailView):
     model = Menu
     template_name = 'menus/detail.html'
+
 
 class MenuRecipeDetailView(generic.DetailView):
     model = Menu
     template_name = 'menus/MenuRecipeDetail.html'
 
-class AddMenuView(generic.FormView):
+
+class MenuCreateView(CreateView):
+    model = Menu
     template_name = 'menus/add_menu.html'
-    form_class = ContactForm
-    success_url = '/cookbook/menus'
-
-    def form_valid(self, form):
-        # This method is called when valid form data has been POSTed.
-        # It should return an HttpResponse.
-        form.send_email()
-        return super().form_valid(form)
-
-
+    fields = ['name', 'recipes', 'servings']
