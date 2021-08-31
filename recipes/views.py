17e0cca62e3943d2b9b9d9919f802bd570bdd799
Lene Lefiti <47546203+LenLen684@users.noreply.github.com>
2021-08-30 23:54:09 -0600

    Finished fixing format
    Just need to change the format one last time now.

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import generic
 from recipes.models import Recipe
 
+import json
+
 # Create your views here.
 class IndexView(generic.ListView):
     model = Recipe
@@ -11,3 +13,8 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Recipe
     template_name = 'recipes/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['stepsAndIngredients'] = json.loads(context['recipe'].stepsAndIngredients)
+        return context
