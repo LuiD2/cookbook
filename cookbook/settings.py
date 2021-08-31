c328a05653a7a1fa43d8147380301db0fd3017b7
LuiD2 <47342745+LuiD2@users.noreply.github.com>
2021-08-30 19:42:50 -0600

    Initial Initialization of the API Application
    createapp of api. Pip installed djangorestframework and django-cors-headers. Restframework provides the framework for APIs and cors-headers allows for outside access to the API. I added these apps into the settings.py.

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -39,10 +39,16 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_extensions',
     'recipes', 
-    'menus'
+    'menus',
+    'rest_framework',
+    'corsheaders',
+    'api'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
